5aa9245fc555623368d4f65e77edbdb5099d8399
Raindrop <635880154@qq.com>
2024-04-25 13:43:16 +0800

    feature(v1.0.0 Raindrop): 初始化信息

---
+++ src/main/java/cn/raindropair/easysocket/constants/BaseCons.java
@@ -0,0 +1,47 @@
+package cn.raindropair.easysocket.constants;
+
+/**
+ * @description EasySocket常量
+ * @Date 2024/4/3
+ * @Author raindrop
+ */
+public class BaseCons {
+    /**
+     * 登陆的Key
+     */
+    public static final String KEY = "key";
+
+    /**
+     * 消息业务类型
+     * heart 心跳检测
+     */
+    public static final String BIZ_HEART = "heart";
+
+    /**
+     * 消息业务类型
+     * heart 响应
+     */
+    public static final String BIZ_RESPONSE = "response";
+
+    /**
+     * 消息业务类型
+     * heart 心跳检测
+     */
+    public static final String BIZ_CONNECT = "connect";
+
+    /**
+     * ping pong 消息
+     */
+    public static final String PING = "ping";
+    public static final String PONG = "pong";
+
+    /**
+     * 请求超时提示
+     */
+    public static final String TIMEOUT_MSG = "请求超时";
+
+
+    public static final String REQUEST_ATTR_USERNAME = "username";
+
+    public static final String REQUEST_ATTR_PASSWORD = "password";
+}
